1dfd9e1bb2609ccbc860a7c7cbcdcb5f1769cc50
Lior Shem-Tov <liorst187@gmail.com>
2016-12-08 16:03:30 +0200

    setup_tmux - adding tmuxifier

--- setup_tmux.sh
+++ setup_tmux.sh
@@ -1,1 +1,7 @@
-git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
+TMUX_PATH = ${HOME}/.tmuxifier
+git clone https://github.com/jimeh/tmuxifier.git $TMUX_PATH
+if [[ ! ":$PATH:" == *":$TMUX_PATH/bin:"* ]]; then
+	export $PATH="$TMUX_PATH/bin:$PATH"
+fi
+
+
